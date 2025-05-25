5586266a086d3a555b7553595f086cdfad3e94b8
aa2789 <aa2789@njit.edu>
2025-05-25 12:49:23 -0400

    Removing uneeded files

--- build/.qt/appLazyFileBrowserQML_qml.cmake
+++
@@ -1,9 +0,0 @@
-
-set(target "appLazyFileBrowserQML")
-set(working_dir "C:/Users/17323/Desktop/QTProjs/LazyFileBrowserQML")
-set(src_and_dest_list
-    "C:/Users/17323/Desktop/QTProjs/LazyFileBrowserQML/Main.qml"
-    "C:/Users/17323/Desktop/QTProjs/LazyFileBrowserQML/build/LazyFileBrowserQML/Main.qml"
-
-)
-set(timestamp_file "C:/Users/17323/Desktop/QTProjs/LazyFileBrowserQML/build/.qt/appLazyFileBrowserQML_qml.txt")
--- build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/.qt/appLazyFileBrowserQML_qml.cmake
+++
@@ -1,9 +0,0 @@
-
-set(target "appLazyFileBrowserQML")
-set(working_dir "C:/Users/17323/Desktop/QTProjs/LazyFileBrowserQML")
-set(src_and_dest_list
-    "C:/Users/17323/Desktop/QTProjs/LazyFileBrowserQML/Main.qml"
-    "C:/Users/17323/Desktop/QTProjs/LazyFileBrowserQML/build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/LazyFileBrowserQML/Main.qml"
-
-)
-set(timestamp_file "C:/Users/17323/Desktop/QTProjs/LazyFileBrowserQML/build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/.qt/appLazyFileBrowserQML_qml.txt")
